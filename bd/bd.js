05ed6a0d471ebf5581f686ae245d74520e9fafd3
gabrielaperezweb <gabrielaperezs21122006@gmail.com>
2025-11-05 19:28:09 -0600

    Preparado para Render

--- bd/bd.js
+++ bd/bd.js
@@ -1,14 +1,16 @@
-import mongoose from "mongoose"
-import 'dotenv/config'
+// bd/bd.js
+import mongoose from "mongoose";
+import 'dotenv/config'; // Carga las variables de entorno automáticamente
+
 async function conectarBD() {
-	try{
-	const respuestaMongo =await mongoose.connect(process.env.SECRET_MONGO)	
-	//mongoose.connect("mongodb://localhost:27017/backend1")
-	console.log("Conexion con MongoDB Atlas")
-	}
-	catch(err){
-		console.log("Error"+err)
-	}
+  try {
+    const respuestaMongo = await mongoose.connect(process.env.SECRET_MONGO);
+    // Para pruebas locales, puedes usar:
+    // mongoose.connect("mongodb://localhost:27017/backend1")
+    console.log(" Conexion con MongoDB Atlas");
+  } catch (err) {
+    console.log(" Error: " + err);
+  }
 }
 
-export default conectarBD
+export default conectarBD;
